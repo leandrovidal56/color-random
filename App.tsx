2bb372fc7fce7d852e0c1c256dcecb5bfb672fd8
Leandrovidal56 <leandrovidal56@gmail.com>
2025-02-24 09:54:45 -0300

    feat: clear code

--- App.tsx
+++ App.tsx
@@ -1,20 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState('#fff');
+  const [textColor, setTextColor] = useState('#f35');
+
+  const colorInterval = useRef<NodeJS.Timeout | null>(null);
+
+  const generateColor = () =>
+    `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, '0')}`;
+
+  const handlePress = () => {
+    setBackgroundColor(generateColor());
+    setTextColor(generateColor());
+  };
+
+  const startColorAnimation = () => {
+    if (!colorInterval.current) {
+      colorInterval.current = setInterval(() => {
+        setBackgroundColor(generateColor());
+        setTextColor(generateColor());
+      }, 300);
+    }
+  };
+
+  const stopColorAnimation = () => {
+    if (colorInterval.current) {
+      clearInterval(colorInterval.current);
+      colorInterval.current = null;
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <TouchableOpacity
+      onPress={handlePress}
+      onPressIn={startColorAnimation}
+      onPressOut={stopColorAnimation}
+      style={[styles.container, { backgroundColor }]}
+    >
+      <Text style={[styles.text, { color: textColor }]}>Hello there</Text>
       <StatusBar style="auto" />
-    </View>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
